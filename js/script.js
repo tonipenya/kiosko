41613a530f04eb087c46822ee046ec0927f946d5
Toni Penya-Alba <tonipenya@iiia.csic.es>
2012-11-19 11:01:46 +0100

    A bit of a cleanup.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,4 @@
 $(function() {
-    var $dom;
-    
     var myurl="http://kiosko.net/es/general.html";
     //make the call to YQL 
     $.getJSON("http://query.yahooapis.com/v1/public/yql?"+
@@ -8,30 +6,26 @@ $(function() {
                 encodeURIComponent(myurl)+
                 "%22&format=xml'&callback=?",
         function(data){
-          if(data.results[0]){
-            //this data.results[0] is the return object you work with, 
-            //if you actually want to do something with the returned json
-            dom = $('<div>').html(data.results[0]);
-            $('.thcover > img', dom).each(function(index) {
-                $('#content').append("<a href='" + $(this).attr("src").replace("200", "750") + "' target='_blank'><img class='frontpage' src='" + $(this).attr("src").replace("200", "300") + "'/></a>");
-            });
-
-            var $container = $('#content');
-            $container.imagesLoaded( function(){
-                $container.masonry({
-                    itemSelector : 'img',
-                    isFitWidth: true
-                });
-            });
-          } else {
-            var errormsg = 'Error: could not load the page.';
-            //output to firebug's console
-            //use alert() for other browsers/setups
-            conole.log(errormsg);
-          }
+            if(data.results[0]){
+                addFirstPages(data.results[0]);
+                applyMasonry();
+            } else {
+                var errormsg = 'Error: could not load the page.';
+                conole.log(errormsg);
+            }
         }
       );
-/*
+});
+
+function addFirstPages(data) {
+    var $dom;
+    dom = $('<div>').html(data);
+    $('.thcover > img', dom).each(function(index) {
+        $('#content').append("<a href='" + $(this).attr("src").replace("200", "750") + "' target='_blank'><img class='frontpage' src='" + $(this).attr("src").replace("200", "300") + "'/></a>");
+    });
+}
+
+function applyMasonry() {
     var $container = $('#content');
     $container.imagesLoaded( function(){
         $container.masonry({
@@ -39,18 +33,4 @@ $(function() {
             isFitWidth: true
         });
     });
-  */  
-
-/*
-    $.ajax({
-        url : "http://kiosko.net/es/general.html",
-        success : function(result){
-            dom = $('<div>').html(result);
-            $('.thcover > img', dom).each(function(index) {
-                $('#content').append("<a href='" + $(this).attr("src").replace("200", "750") + "' target='_blank'><img class='frontpage' src='" + $(this).attr("src").replace("200", "300") + "'/></a>");
-            });
-        }
-    });
-    */
-});
-
+}
